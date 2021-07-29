0b1f8fc6e4bfa23854317d921adf08884b814bff
瑄 <apet4032689@gmail.com>
2021-07-29 15:06:01 +0800

    移動

---
+++ WebFormMiniExample/AccountingNote.DBSource/UserInfoManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingNote.DBSource
+{
+
+    public class UserInfoManager
+    {
+        private static string GetConnectionString()
+        {
+            string val = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            return val;
+        }
+        public static DataRow GetUserInfoByAccount(string account)
+        {
+            string connectionString = GetConnectionString();
+            string dbCommandStirng =
+                @"SELECT [ID], [Account], [PWD], [Name], [Email]
+                    FROM UserInfo    
+                    WHERE [Account] = @account
+                ";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand comand = new SqlCommand(dbCommandStirng, connection))
+                {
+                    comand.Parameters.AddWithValue("@account", account);
+
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = comand.ExecuteReader();
+
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+                        reader.Close();
+
+                        // 假設是0筆資料，回傳null                     
+                        if (dt.Rows.Count == 0)
+                            return null;
+
+                        // 假設是不是0筆，回傳資料
+                        DataRow dr = dt.Rows[0];
+                        return dr;
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.WriteLog(ex);
+                        return null;
+                    }
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ WebFormMiniExample/AccountingNote/SystemAdmin/UserInfo.aspx.cs
@@ -0,0 +1,45 @@
+﻿using AccountingNote.DBSource;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AccountingNote.SystemAdmin
+{
+    public partial class UserInfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)  // 可能是按鈕跳回本頁，所以要判斷 postback
+            {
+                if (this.Session["UserLoginInfo"] == null)  // 如果尚未登入，導入登入頁
+                {
+                    Response.Redirect("/Login.aspx");
+                    return;
+                }
+
+                string account = this.Session["UserLoginInfo"] as string;
+                DataRow dr = UserInfoManager.GetUserInfoByAccount(account);
+
+                if (dr == null)    // 帳號不存在，導入登入頁
+                {
+                    this.Session["UserLoginInfo"] = null;
+                    Response.Redirect("/Login.aspx");
+                    return;
+                }
+
+                this.liAccount.Text = dr["Account"].ToString();
+                this.ltName.Text = dr["Name"].ToString();
+                this.ltEmail.Text = dr["Email"].ToString();
+            }
+        }
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Session["UserLoginInfo"] = null;  // 清初登入資訊，導入登入頁
+            Response.Redirect("/Login.aspx");
+        }
+    }
+}
