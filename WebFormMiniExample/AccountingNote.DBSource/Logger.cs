c742d384b446e25ed318f1791f54d1f1c8983989
瑄 <apet4032689@gmail.com>
2021-07-30 13:14:48 +0800

    【修改】修改Logger路徑內容

--- WebFormMiniExample/AccountingNote.DBSource/Logger.cs
+++ WebFormMiniExample/AccountingNote.DBSource/Logger.cs
@@ -16,6 +16,15 @@ namespace AccountingNote.DBSource
 
                 ";
 
+            string logPath = "D:\\Logs\\Log.log";
+            string folderPath = System.IO.Path.GetDirectoryName(logPath);
+
+            if (!System.IO.Directory.Exists(folderPath))
+                System.IO.Directory.CreateDirectory(folderPath);
+
+            if (!System.IO.File.Exists(logPath))
+                System.IO.File.Create(logPath);
+
             System.IO.File.AppendAllText("D:\\Logs\\Log.log", msg);
 
             throw ex;
